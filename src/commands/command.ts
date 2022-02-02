275388df4654d282a7034fad6d3c57c014c20224
r3s <rahul.es@outlook.com>
2022-02-02 12:04:58 +0530

    When hashing multiple selections, Undo only once to revert to previous state.

--- src/commands/command.ts
+++ src/commands/command.ts
@@ -7,12 +7,13 @@ export const textCommand =  async (method: Function) => {
         const document = editor.document;
         const selections = editor.selections;
         // Get the word within the selection
+        
         for (const selection of selections) {
             const word = document.getText(selection);
             const encoded = method(word);
             await editor.edit(editBuilder => {
                 editBuilder.replace(selection, encoded);
-            });
+            }, { undoStopBefore: false, undoStopAfter: false });
         }
     }
 };
